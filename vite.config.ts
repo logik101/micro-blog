9b3218318a53e7c73cea3dd7e51604e62f491614
logico <43566742+logik101@users.noreply.github.com>
2025-12-31 02:49:54 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/micro-blog/',
+    base: './',
     plugins: [react()],
     server: {
       port: 3000,
